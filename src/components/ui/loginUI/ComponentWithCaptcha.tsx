131517d114ca004d23ce0e4f94f4966a61b2b48c
Anton Volkov <66030600+AHTOHFANTOM@users.noreply.github.com>
2025-07-13 18:32:39 +0300

    Добавил логи для каптчи от яндекса

--- src/components/ui/loginUI/ComponentWithCaptcha.tsx
+++ src/components/ui/loginUI/ComponentWithCaptcha.tsx
@@ -1,22 +1,30 @@
 'use client';
 
 import { SmartCaptcha } from '@yandex/smart-captcha';
-import { CAPTCHA_SITE_KEY } from "@/components/types/constants";
+import { CAPTCHA_SITE_KEY, CAPTCHA_VERIFY_URL } from "@/components/types/constants";
 import styles from "@/styles/start/start.module.css";
 
 export const ComponentWithCaptcha = ({ setIsCaptchaVerified }: { setIsCaptchaVerified: (b: boolean) => void }) => {
 
     const handleSuccess = async (token: string) => {
+        console.debug("[CAPTCHA] Token received:", token);
+
         try {
-            const res = await fetch("http://217.199.252.124:8080/verify", {
+            console.debug("[CAPTCHA] Пробую отправить запрос по адресу" + CAPTCHA_VERIFY_URL);
+            const res = await fetch(CAPTCHA_VERIFY_URL, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ token }),
             });
+
+            console.debug("[CAPTCHA] Verification request sent to backend");
+
             const data = await res.json();
+            console.info("[CAPTCHA] Backend response:", data);
+
             setIsCaptchaVerified(data.success);
         } catch (e) {
-            console.error(e);
+            console.error("[CAPTCHA] Error verifying token with backend:", e);
             setIsCaptchaVerified(false);
         }
     };
