19130e55dd210b7d6d12ba6ab710cdb8473a5d52
Anton Volkov <66030600+AHTOHFANTOM@users.noreply.github.com>
2025-07-13 18:10:18 +0300

    Правка лого на главном page.tsx

--- src/components/types/constants.ts
+++ src/components/types/constants.ts
@@ -1,3 +1,3 @@
-export const CAPTCHA_VERIFY_URL = 'http://localhost:8000/verify';
+export const CAPTCHA_VERIFY_URL = 'http://217.199.252.124:8080/verify';
 export const CAPTCHA_SITE_KEY = 'ysc1_b2m478OAwSjUbVXqWBRYQsecrhePh8IZygjxTTYhbe77d021';
 export const TEST_CODE = '123456';
