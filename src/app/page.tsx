b8b26282a52f089841fbafde17dd44b8879910e6
Anton Volkov <66030600+AHTOHFANTOM@users.noreply.github.com>
2025-07-21 16:00:54 +0300

    Изменил главную страницу на пк и на мобилке

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,6 +25,32 @@ export default function Home() {
     return (
         <div className="wrapper">
           <div className="dev-container">
+            <main className={"main-dev-container"}>
+              <div className="logo">
+                <Image className={styles.logoCheckRfIcon} src={"/logo.svg"} alt={"Логотип Чек24.рф"} width={81} height={95}/>
+                <div className="title">
+                  <h1>ЧЕК24.РФ</h1>
+                  <p>цифровая платформа</p>
+                </div>
+              </div>
+              <div className="dev-mode">
+                <span>Этот сервис предназначен для использования на компьютере.</span>
+                <span> Пожалуйста, откройте сайт на ПК или ноутбуке для полноценного доступа к функционалу.</span>
+              </div>
+            </main>
+            <footer className="footer-dev-container">
+              <p>Возможно мобильная версия появится позже</p>
+            </footer>
+          </div>
+        </div>
+    );
+  }
+
+  return (
+      <div className="wrapper">
+        <div className={styles.backgroundImg}/>
+        <div className="dev-container">
+          <main className={"main-dev-container"}>
             <div className="logo">
               <Image className={styles.logoCheckRfIcon} src={"/logo.svg"} alt={"Логотип Чек24.рф"} width={81} height={95}/>
               <div className="title">
@@ -33,28 +59,12 @@ export default function Home() {
               </div>
             </div>
             <div className="dev-mode">
-              <span>Этот сервис предназначен для использования на компьютере.</span>
-              <span> Пожалуйста, откройте сайт на ПК или ноутбуке для полноценного доступа к функционалу.</span>
+              <h1>Сайт находится в <br/> разработке.</h1>
             </div>
-          </div>
-        </div>
-    );
-  }
-
-
-  return (
-      <div className="wrapper">
-        <div className="dev-container">
-          <div className="logo">
-            <Image className={styles.logoCheckRfIcon} src={"/logo.svg"} alt={"Логотип Чек24.рф"} width={81} height={95}/>
-            <div className="title">
-              <h1>ЧЕК24.РФ</h1>
-              <p>цифровая платформа</p>
-            </div>
-          </div>
-          <div className="dev-mode">
-            <h1>Сайт находится в <br/> разработке.</h1>
-          </div>
+          </main>
+          <footer className="footer-dev-container">
+            <p>&copy; ЧЕК24.РФ - зарегистрированная торговая марка ООО "ГИП". 2024-2025</p>
+          </footer>
         </div>
       </div>
   );
