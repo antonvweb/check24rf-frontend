01e6bb7930a7ebb5f560e600d17bdb1c089175cc
Anton Volkov <66030600+AHTOHFANTOM@users.noreply.github.com>
2025-07-23 01:16:09 +0300

    1

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig: import('next').NextConfig = {
     turbopack: {},
-    async redirects() {
-        return [
-            {
-                source: '/(.*)',
-                has: [{ type: 'host', value: 'чек24.рф' }],
-                destination: 'http://чек24.рф/:path*',
-                permanent: true,
-            },
-        ]
-    },
 };
 
 export default nextConfig;
