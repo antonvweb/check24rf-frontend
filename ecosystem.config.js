98d01dbe2c014f1d771afd853ddf33a37267f675
Anton Volkov <66030600+AHTOHFANTOM@users.noreply.github.com>
2025-07-22 19:22:50 +0300

    122

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
             name: "check24rf-ip",             // имя процесса в PM2
             script: "npm",                   // запускаем npm
             args: "run start:ip",            // с этими аргументами (npm run start:ip)
-            cwd: "/path/to/your/project",    // рабочая директория проекта (замени на свой путь)
+            cwd: "/var/www/check24rf/dev/",    // рабочая директория проекта (замени на свой путь)
             output: "/var/log/check24rf-ip.log",     // куда писать стандартный вывод (stdout)
             error: "/var/log/check24rf-ip.err.log",  // куда писать ошибки (stderr)
             merge_logs: true,                // объединять ли stdout и stderr в один поток (можно true/false)
