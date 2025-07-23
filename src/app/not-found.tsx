b9563b45a4bbe5c019c6df28c7c43df971e6d99b
Anton Volkov <66030600+AHTOHFANTOM@users.noreply.github.com>
2025-07-23 18:54:34 +0300

    для домена теперь показывает только разработку

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,24 +1,12 @@
-import Link from 'next/link';
-import Logo from "@/components/Logo";
-import styles from "@/styles/start/start.module.css";
-import React from "react";
+import {useEffect} from "react";
+import {useRouter} from "next/navigation";
 
 export default function NotFound() {
-    return (
-        <div className="notFound">
-            <div className={styles.backgroundImg}/>
-            <main className={"mainNotFound"}>
-                <div className="notFoundlogo">
-                    <Logo/>
-                </div>
-                <div className="centerNotFound">
-                    <div className="notFoundTitle">
-                        <span>404</span>
-                        <span>Страница которую вы ищите не существует</span>
-                    </div>
-                    <Link href="/start" className={"backCite"}>Вернуться на главную</Link>
-                </div>
-            </main>
-        </div>
-    );
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push('/');
+    }, [router]);
+
+    return null;
 }
